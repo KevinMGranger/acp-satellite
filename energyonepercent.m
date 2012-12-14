85d439b0c31b4b2e5f65bd1b26a8d4991e38b983
Kevin Granger <kmg2728@rit.edu>
2012-12-14 01:39:41 -0500

    doc for energyonepercent

--- energyonepercent.m
+++ energyonepercent.m
@@ -1,9 +1,24 @@
 function energyonepercent
-% #occupymatlab
+
+%{
+    NAME
+    	energyonepercent -- find the highest possible timestep that still results in a ~1% fractional error
+
+    DESCRIPTION
+        Find the fractional error for a simulation given certain starting values,
+	then use the bisection algorithim to get as close as possible.
+	
+	We are the 1%. #occupymatlab
+
+    AUTHOR
+        Kevin Granger -- kmg2728@rit.edu
+%}
 
 START_ALTITUDE = 250000 % m
 START_VELOCITY = 7762 % m/s
+% this value was found to be a good starting point when running the other energy wrapper.
 upper_timestep = timestep = 0.95;
+% 0 is a good lower bound for bisection. essentially just halving it, but only for the first run.
 lower_timestep = 0;
 
 tries = 0;
@@ -25,4 +40,3 @@ end
 if (tries == 10)
 	fprintf('NOTE: Tries exceeded.\n');
 end
-
